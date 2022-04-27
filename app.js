d2b11a1166a4f9e310c2ecdc1ae5cc56e3366c00
Ricardo Rodriguez <rrodriguez@insideone.com.ar>
2022-04-27 11:12:37 -0300

    Tareas.json was reset and colors were added to the list

--- app.js
+++ app.js
@@ -7,48 +7,80 @@ const main = async () => {
   let opt = "";
   let tareas = new Tareas();
   do {
-    console.clear()
+    console.clear();
     opt = await inquirerMenu();
     switch (opt.option) {
       case "1":
         const desc = await leerInput("descripcion: ");
-        tareas.crear(desc)
-        fs.writeFileSync("./Tareas.json",JSON.stringify(tareas.__listado),{encoding:"utf8"})
-
+        tareas.crear(desc);
+        fs.writeFileSync("./Tareas.json", JSON.stringify(tareas.__listado), {
+          encoding: "utf8",
+        });
         break;
-        case "2":
-      console.log(tareas.__listado)
+      case "2":
+        tareas.__listado.forEach((Element) => {
+          if (Element.completado == true) {
+            console.log(
+              `id: ${Element.id.green} Description: ${
+                Element.desc.green
+              } State: ${"Completed".green} \n`
+            );
+          } else {
+            console.log(
+              `id: ${Element.id.green} Description: ${
+                Element.desc.green
+              } State: ${"Uncompleted".red}\n`
+            );
+          }
+        });
+
         break;
       case "3":
         tareas.__listado.forEach((Element) => {
           if (Element.completado == true) {
-            console.log(Element);
+            console.log(
+              `id: ${Element.id.green} Description: ${
+                Element.desc.green
+              } State: ${"Completed".green} \n`
+            );
           }
         });
         break;
       case "4":
         tareas.__listado.forEach((Element) => {
           if (Element.completado != true) {
-            console.log(Element);
+            console.log(
+              `id: ${Element.id.green} Description: ${
+                Element.desc.green
+              } State: ${"Uncompleted".red}\n`
+            );
           }
         });
         break;
       case "5":
         tareas.__listado.forEach((Element) => {
           if (Element.completado != true) {
-            console.log(Element);
+            console.log(
+              `id: ${Element.id.green} Description: ${
+                Element.desc.green
+              } State: ${"Uncompleted".red}\n`
+            );
           }
         });
         const descr = await leerInput("descripcion de la tarea a completar: ");
         tareas.completar(descr);
-        fs.writeFileSync("./Tareas.json",JSON.stringify(tareas.__listado),{encoding:"utf8"})
-       break;
-       case "6":
-        console.log(tareas.__listado)
-         const descri = await leerInput("Que tarea quieres eliminar: ")
-         tareas.eliminar(descri)
-         fs.writeFileSync("./Tareas.json",JSON.stringify(tareas.__listado),{encoding:"utf8"})
-      }
+        fs.writeFileSync("./Tareas.json", JSON.stringify(tareas.__listado), {
+          encoding: "utf8",
+        });
+        break;
+      case "6":
+        console.log(tareas.__listado);
+        const descri = await leerInput("Que tarea quieres eliminar: ");
+        tareas.eliminar(descri);
+        fs.writeFileSync("./Tareas.json", JSON.stringify(tareas.__listado), {
+          encoding: "utf8",
+        });
+    }
     await pausa(opt.option);
   } while (opt.option != "0");
 };
